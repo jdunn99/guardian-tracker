73449208c9eaac1aaf6a6a82dd4938c581fe9dd8
Jack Dunn <jmhjdunn123@outlook.com>
2024-05-10 10:51:01 -0400

    more milestone work

--- app/(profile)/[membershipType]/[membershipId]/[characterId]/(pages)/milestones/_components/milestone-container.tsx
+++ app/(profile)/[membershipType]/[membershipId]/[characterId]/(pages)/milestones/_components/milestone-container.tsx
@@ -9,37 +9,82 @@ import {
   TooltipTitle,
 } from "@/components/ui/tooltip";
 import { useManifest } from "@/lib/manifest/useManifest";
+import { cn } from "@/lib/utils";
 import { HoverCardTrigger } from "@radix-ui/react-hover-card";
 import {
+  AllDestinyManifestComponents,
+  DestinyActivityDefinition,
+  DestinyActivityTypeDefinition,
   DestinyCharacterProgressionComponent,
+  DestinyManifestSlice,
   DestinyMilestone,
+  DestinyMilestoneDefinition,
   DestinyMilestoneQuest,
   DestinyPublicMilestone,
+  DestinyPublicMilestoneChallengeActivity,
   DestinyQuestStatus,
 } from "bungie-api-ts/destiny2";
 import Image from "next/image";
 import React from "react";
 
-type Activity = {
-  challenge?: number;
-  modifiers: number[];
-  hash: number;
+type ParsedMilestone = {
+  category: string;
+  description: string;
+  icon: string;
+  activities: DestinyActivityDefinition[];
 };
 
-type Quest = {
-  hash: number;
-  status: DestinyQuestStatus;
-};
+// function parseActivites(
+//   manifest: DestinyManifestSlice<(keyof AllDestinyManifestComponents)[]>,
+//   activities: DestinyPublicMilestoneChallengeActivity[],
+//   destinyMilestone: DestinyMilestoneDefinition
+// ) {
+//   for (const activity of activities) {
+//     const destinyActivity =
+//       manifest.DestinyActivityDefinition[activity.activityHash];
 
-type ParsedMilestone = Record<
-  number,
-  {
-    quests: Quest[];
-    activities: Activity[];
-  }
->;
+//     if (
+//       destinyMilestone.displayProperties.name === "Weekly Ritual Challenge" ||
+//       !destinyActivity.directActivityModeHash
+//     ) {
+//       continue;
+//     }
+
+//     const activityType =
+//       manifest.DestinyActivityModeDefinition[
+//         destinyActivity.directActivityModeHash
+//       ];
+
+//     activityTypes[activityType.displayProperties.name] =
+//       activityType.displayProperties.icon;
+
+//     // if (q[activityType.displayProperties.name]) {
+//     //   q[activityType.displayProperties.name] = [
+//     //     ...q[activityType.displayProperties.name],
+//     //     destinyMilestone.displayProperties.name,
+//     //   ];
+//     // } else {
+//     //   q[activityType.displayProperties.name] = [
+//     //     destinyMilestone.displayProperties.name,
+//     //   ];
+//     // }
+//   }
+// }
 
 // Raid, Dungeon, Crucible, Gambit, Story, Strike, Scored Nightfall Strike
+// TYQTQ18J7JR15T
+
+type Category = {
+  description: string;
+  icon: string;
+};
+
+type Milestone = {
+  icon: string;
+  name: string;
+  description: string;
+  activities: DestinyPublicMilestoneChallengeActivity[];
+};
 
 export function Milestones({
   milestones,
@@ -55,13 +100,18 @@ export function Milestones({
     if (!manifest) {
       return null;
     }
-    const activityTypes: Record<string, string> = {};
-    const q: Record<string, string[]> = {};
+
+    const activityTypes: Record<string, Category> = {};
+    const milestonesByKey: Record<string, Milestone[]> = {};
+    const quests: Milestone[] = [];
 
     for (const key of Object.keys(publicMilestones)) {
       const milestone = publicMilestones[parseInt(key)];
       const destinyMilestone =
         manifest.DestinyMilestoneDefinition[milestone.milestoneHash];
+
+      let name = "";
+
       if (milestone.activities) {
         for (const activity of milestone.activities) {
           const destinyActivity =
@@ -69,78 +119,134 @@ export function Milestones({
 
           if (
             destinyMilestone.displayProperties.name ===
-            "Weekly Ritual Challenge"
+              "Weekly Ritual Challenge" ||
+            !destinyActivity.directActivityModeHash
           )
             continue;
 
-          if (destinyActivity.directActivityModeHash) {
-            const activityType =
-              manifest.DestinyActivityModeDefinition[
-                destinyActivity.directActivityModeHash
-              ];
+          const activityType =
+            manifest.DestinyActivityModeDefinition[
+              destinyActivity.directActivityModeHash
+            ];
 
-            activityTypes[activityType.displayProperties.name] =
-              activityType.displayProperties.icon;
+          name = activityType.displayProperties.name;
 
-            if (q[activityType.displayProperties.name]) {
-              q[activityType.displayProperties.name] = [
-                ...q[activityType.displayProperties.name],
-                destinyMilestone.displayProperties.name,
-              ];
-            } else {
-              q[activityType.displayProperties.name] = [
-                destinyMilestone.displayProperties.name,
-              ];
-            }
-          }
+          activityTypes[name] = {
+            ...activityType.displayProperties,
+          };
         }
+
+        if (milestonesByKey[name]) {
+          milestonesByKey[name] = [
+            ...milestonesByKey[name],
+            {
+              activities: milestone.activities,
+              ...destinyMilestone.displayProperties,
+            },
+          ];
+        } else {
+          milestonesByKey[name] = [
+            {
+              activities: milestone.activities,
+              ...destinyMilestone.displayProperties,
+            },
+          ];
+        }
+      }
+
+      if (milestone.availableQuests) {
+        const [quest] = milestone.availableQuests;
+
+        const destinyQuest =
+          manifest.DestinyInventoryItemDefinition[quest.questItemHash];
+
+        quests.push({
+          activities: [],
+          ...destinyQuest.displayProperties,
+        });
       }
     }
 
-    return { activityTypes, q };
+    return { activityTypes, milestonesByKey, quests };
   }, [manifest, publicMilestones]);
 
   return (
-    <div className="w-full container mx-auto">
-      <div className="w-full flex  py-32 gap-4">
-        <ul className="flex flex-col gap-1">
-          {data &&
-            Object.keys(data.activityTypes).map((type) => (
-              <li
-                key={type}
-                className="p-0.5 transition-all border border-transparent hover:border-slate-700"
-                onClick={() => setSelected(type)}
-              >
-                <HoverCard openDelay={300} closeDelay={300}>
-                  <HoverCardTrigger asChild>
-                    <Image
-                      src={`https://bungie.net${data.activityTypes[type]}`}
-                      alt=""
-                      width={48}
-                      height={48}
-                      className="border p-1 border-slate-700 bg-slate-800/50 hover:bg-slate-700/50 text-slate-300"
-                    />
-                  </HoverCardTrigger>
-                  <DestinyTooltip>
-                    <TooltipHeader className="bg-slate-700/30">
-                      <TooltipTitle>{type}</TooltipTitle>
-                    </TooltipHeader>
-                    <TooltipBody>
-                      <TooltipDescription>Tis</TooltipDescription>
-                    </TooltipBody>
-                  </DestinyTooltip>
-                </HoverCard>
-              </li>
-            ))}
-        </ul>
-        <div className="flex gap-2 w-full p-0.5">
-          <div className="grid grid-cols-2 w-full gap-1">
-            {selected &&
-              data &&
-              data.q[selected].map((item) => (
-                <p className="text-white" key={crypto.randomUUID()}>
-                  {item}
-                </p>
+    <div className="w-full container mx-auto py-32">
+      <div className="grid grid-cols-4 gap-4">
+        <div className="col-span-3">
+          <h5 className="text-xs uppercase text-yellow-500 font-bold pb-4">
+            Activities - {selected}
+          </h5>
+          <div className="flex items-start w-full gap-4">
+            <ul className="flex flex-col gap-1">
+              {data &&
+                Object.keys(data.activityTypes).map((type) => (
+                  <li
+                    key={type}
+                    className="p-0.5 transition-all border border-transparent hover:border-slate-700"
+                    onClick={() => setSelected(type)}
+                  >
+                    <HoverCard openDelay={300} closeDelay={300}>
+                      <HoverCardTrigger asChild>
+                        <Image
+                          src={`https://bungie.net${data.activityTypes[type].icon}`}
+                          alt=""
+                          width={48}
+                          height={48}
+                          className={cn(
+                            "border p-1  hover:bg-slate-700/50 text-slate-300",
+                            selected === type
+                              ? "bg-slate-700/50 border-yellow-500/40"
+                              : "bg-slate-800/50 border-slate-700"
+                          )}
+                        />
+                      </HoverCardTrigger>
+                      <DestinyTooltip>
+                        <TooltipHeader className="bg-slate-700/30">
+                          <TooltipTitle>{type}</TooltipTitle>
+                        </TooltipHeader>
+                        <TooltipBody>
+                          <TooltipDescription>
+                            {data.activityTypes[type].description}
+                          </TooltipDescription>
+                        </TooltipBody>
+                      </DestinyTooltip>
+                    </HoverCard>
+                  </li>
+                ))}
+            </ul>
+            <div className="grid grid-cols-2 w-full gap-2 ">
+              {selected &&
+                data &&
+                data.milestonesByKey[selected].map((item) => (
+                  <div
+                    className="w-full bg-slate-800/30 border border-slate-700/50 px-8 py-4"
+                    key={crypto.randomUUID()}
+                  >
+                    <p className="text-white" key={crypto.randomUUID()}>
+                      {item.name}
+                    </p>
+                    <p className="text-sm text-slate-400">{item.description}</p>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+        <div>
+          <h5 className="text-xs uppercase text-yellow-500 font-bold pb-4">
+            Quests
+          </h5>
+          <div className="flex w-64 flex-wrap gap-2">
+            {data &&
+              data.quests.map((item) => (
+                <Image
+                  className="bg-slate-800/50  border-slate-700 border p-1"
+                  key={item.name}
+                  width={48}
+                  height={48}
+                  src={`https://bungie.net${item.icon}`}
+                  alt=""
+                />
               ))}
           </div>
         </div>
